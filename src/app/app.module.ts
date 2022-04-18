fe7460f648e54d8f2159138bd7d7d17da274faed
iagogermano <iagogermano15@gmail.com>
2022-04-18 17:08:23 -0300

    commit 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { myfirstcomponent } from'./myfirst.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    myfirstcomponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/myfirst.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'myfirst',
+    template:'<h1>my first component</h1>'
+    
+})
+export class myfirstcomponent{
+
+}
