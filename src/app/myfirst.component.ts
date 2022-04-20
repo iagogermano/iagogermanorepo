468dde2b3a840ca2263b4d30be5a26bb99985c3c
iagogermano <iagogermano15@gmail.com>
2022-04-20 16:03:45 -0300

    commit7

--- src/app/myfirst.component.ts
+++ src/app/myfirst.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'myfirst',
-    template:'<h1>my first component</h1>'
+    template:'<h1>olá mundo, esse é o meu módulo</h1>'
     
 })
 export class myfirstcomponent{
